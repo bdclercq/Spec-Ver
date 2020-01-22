eb8b30c228129ccbb8d80d50980c57e42b4fe412
Beau De Clercq <bdclercq@iMac-van-Beau.local>
2020-01-22 10:25:17 +0100

    update tests

--- ParityGame/run_tests.sh
+++ ParityGame/run_tests.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+#
+#for filename in /tests/*.txt; do
+#    python PG.py $filename
+#done
+for filename in tests/*.txt; do
+    [ -e "$filename" ] || continue
+    python PG.py $filename
+    # ... rest of the loop body
+done
 
-FILES=/tests/*
-for FILE in $FILES:
-do
-    echo $FILE
-    python PG.py $FILE
+for filename in tests/*.dot; do
+    [ -e "$filename" ] || continue
+    dot -Tpng $filename -o $filename.png
+    # ... rest of the loop body
 done
